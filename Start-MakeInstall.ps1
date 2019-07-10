0fcfb33a2b7739bec16183e239431842d3325b43
Wanli Zhu <wanli.zhu@autodesk.com>
2019-07-10 13:54:08 +0800

    t

--- Start-MakeInstall.ps1
+++
@@ -1,9 +0,0 @@
-& .\Start-BuildEngine.ps1 -CloseShell
-
-& cmake --build .\Build --parallel --target INSTALL --config Debug
-
-if($LASTEXITCODE -NE 0) {
-    Write-Host "!!! 'Make Install' failed"
-}
-
-Pause
