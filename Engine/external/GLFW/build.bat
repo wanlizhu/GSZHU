02b26a682c159afb56192f3403663dbe20eb830f
wanlizhu <wanlizhu@hotmail.com>
2019-08-21 20:13:43 +0800

    0821

--- Engine/external/GLFW/build.bat
+++ Engine/external/GLFW/build.bat
@@ -4,3 +4,4 @@ mkdir build
 cmake -G "Visual Studio 16 2019" -S "." -B "build" -DGLFW_BUILD_DOCS=FALSE -DGLFW_BUILD_EXAMPLES=FALSE -DGLFW_BUILD_TESTS=FALSE -DGLFW_INSTALL=FALSE   
 
 cmake --build build
+
--- cmd_clean_intermediate.bat
+++ cmd_clean_intermediate.bat
@@ -3,5 +3,7 @@
 if exist "bin\" rd /s/q "bin\"
 if exist "lib\" rd /s/q "lib\"
 if exist "Engine\__cache__\"   rd /s/q "Engine\__cache__\"
+if exist "Engine\external\GLFW\build\" rd /s/q "Engine\external\GLFW\build\"
 if exist "Launcher\__cache__\" rd /s/q "Launcher\__cache__\"
 if exist "UnitTest\__cache__\" rd /s/q "UnitTest\__cache__\"
+
