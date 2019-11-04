aeaf10df4d43cc7ed3fe6d2fed0e9b9f3ef1f1dc
wanlizhu <wanlizhu@hotmail.com>
2019-11-04 23:50:46 +0800

    add devcmd.py

--- devcmd.py
+++ devcmd.py
@@ -7,6 +7,22 @@ import importlib.util
 import inspect
 import getpass
 import socket
+import colorama
+
+def redBackText(text):
+    return colorama.Back.RED + colorama.Fore.WHITE + text + colorama.Fore.RESET + colorama.Back.RESET
+
+def redText(text):
+    return colorama.Fore.RED + text + colorama.Fore.RESET
+
+def greenText(text):
+    return colorama.Fore.GREEN + text + colorama.Fore.RESET
+
+def blueText(text):
+    return colorama.Fore.BLUE + text + colorama.Fore.RESET
+
+def yellowText(text):
+    return colorama.Fore.YELLOW + text + colorama.Fore.RESET
 
 def unpackVariable(var):
     translator = {
@@ -28,16 +44,23 @@ def unpackVariable(var):
     
 
 def initEnv():
+    colorama.init(autoreset=True)
     envJson = 'init_env.json'
-    with open(envJson) as jsonFile:
-        data = json.load(jsonFile)
-        for key, value in data.items():
-            key = unpackVariable(key)
-            value = unpackVariable(value)
-            os.environ[key] = value
+    try:
+        with open(envJson) as jsonFile:
+            data = json.load(jsonFile)
+            for key, value in data.items():
+                key = unpackVariable(key)
+                value = unpackVariable(value)
+                os.environ[key] = value
+    except IOError as error:
+        print(redBackText('IOError'), redText('%s' % (error)))
+        os.system('pause')
+        exit(1)
 
 def inputCommand():
-    print(getpass.getuser() + '@' + socket.gethostname() + '$ ', end='')
+    prefix = getpass.getuser() + '@' + socket.gethostname()
+    print(greenText(prefix) + '$ ', end='')
     return input()
 
 def prepareCommand(cmdLine):
@@ -48,20 +71,24 @@ def prepareCommand(cmdLine):
     return cmdName, cmdArgs
 
 def findCommand(cmdName, scriptsPath):
+    cmdFuncName = 'cmd_' + cmdName if '-' not in cmdName else cmdName.replace('-', '_')
     for entry in os.scandir(scriptsPath):
         if entry.is_dir() or entry.name=='__init__.py' or \
                              os.path.splitext(entry.path)[1] not in ['.py', '.pyc']:
             continue
 
-        spec = importlib.util.spec_from_file_location('devcmd', entry.path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[spec.name] = module
-        spec.loader.exec_module(module)
+        try:
+            spec = importlib.util.spec_from_file_location('devcmd', entry.path)
+            module = importlib.util.module_from_spec(spec)
+            sys.modules[spec.name] = module
+            spec.loader.exec_module(module)
+        except Exception as error:
+            print(yellowText(str(error)))
+            continue
 
         for obj in inspect.getmembers(module, inspect.isfunction):
-            if obj[0] == ('cmd_' + cmdName):
+            if obj[0] == cmdFuncName:
                 return getattr(module, obj[0])
-
     return None
 
 
@@ -69,11 +96,16 @@ def dispatchCommand(cmdName, cmdArgs):
     shouldQuit = True if cmdName in ['quit', 'exit'] else False
     if not shouldQuit:
         cmdFunc = findCommand(cmdName, os.environ['SCRIPTS_PATH'])
-        if cmdFunc is not None:
-            cmdFunc(cmdArgs)
-        else: # fallback to run it as system command
-            os.system(cmdName + ' ' + ' '.join(cmdArgs))
-
+        try:
+            if cmdFunc is not None:
+                cmdFunc(cmdArgs)
+            else: # fallback to run it as system command
+                print(yellowText('[forward to system command]'))
+                os.system(cmdName + ' ' + ' '.join(cmdArgs))
+        except RuntimeError as error:
+            print(redBackText('RuntimeError'), redText(str(error)))
+        except Exception as error:
+            print(redBackText('Exception'), redText(str(error)))
     return shouldQuit
 
 if __name__=='__main__':
@@ -81,11 +113,14 @@ if __name__=='__main__':
     shouldQuit = False
     cmdName = None
     cmdArgs = []
-    while not shouldQuit:
-        cmdLine = inputCommand()
-        if not cmdLine:
-            continue
-
-        cmdName, cmdArgs = prepareCommand(cmdLine)
-        shouldQuit = dispatchCommand(cmdName, cmdArgs)
-        
+    try:
+        while not shouldQuit:
+            cmdLine = inputCommand()
+            if not cmdLine:
+                continue
+            cmdName, cmdArgs = prepareCommand(cmdLine)
+            shouldQuit = dispatchCommand(cmdName, cmdArgs)
+    except Exception as error:
+        print(redBackText('Exception'), redText(str(error)))
+        exit(1)
+    exit(0)
--- develop/scripts/aget.py
+++ develop/scripts/aget.py
@@ -1,12 +1,30 @@
 import os
+import colorama
+import argparse
 
-def cmd_sys(args):
-    os.system(' '.join(args))
+def redPrint(text):
+    print(colorama.Fore.RED + text + colorama.Fore.RESET)
+
+def yellowPrint(text):
+    print(colorama.Fore.YELLOW + text + colorama.Fore.RESET)
     
-def cmd_help(args):
-    print('Usage: amake [-h] [-config {debug|release}]')
-    print('             [-iset {intel64|arm64}]')
-    print('             [-target $TARGET_NAME$]')
-    print('             [-gen] [-build] [-ide] [-clean] [-clean-build] [-test]')
-    print('             {win, macos, ios, android}')
-    print('')
+def cmd_aget(argsList):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('platform', nargs=1, choices=['win', 'macos', 'ios', 'android'], help='target platform')
+    parser.add_argument('-i', '--iset', required=False, default='x64', choices=['x64', 'intel64', 'arm64'], help='target instruction set')
+    parser.add_argument('-c', '--config', required=False, default='debug', choices=['debug', 'release'], help='target configuration type')
+    args = parser.parse_args(argsList)
+    #print(args)
+    
+
+def download_package(url, local):
+    pass
+    
+def generate_package(local, buildPath)
+    pass
+    
+def build_package(buildPath):
+    pass
+    
+def install_package(buildPath, dest):
+    pass
