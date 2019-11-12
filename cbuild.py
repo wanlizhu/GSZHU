da2201a0be19f5939d3e60337ab6507994781dfc
wanlizhu <wanlizhu@hotmail.com>
2019-11-12 18:48:57 +0800

    finish conan setup

--- cbuild.py
+++ cbuild.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import getpass
 import socket
 import colorama
@@ -18,8 +19,12 @@ MAGENTA = colorama.Style.BRIGHT + colorama.Back.RESET + colorama.Fore.MAGENTA
 
 
 class ArgumentParserNoInterrupt(argparse.ArgumentParser):
+    def exit(self, status=0, message=None):
+        if status!=0:
+            return self.error('error(%i): %s' % (status, message))
+
     def error(self, message):
-        raise Exception('error: ' + message)
+        print(RED_INV, message, RESET)
 
 
 class Argument:
@@ -29,7 +34,7 @@ class Argument:
 
     def of(self, namespace):
         if hasattr(namespace, self.name):
-            return namespace.name
+            return getattr(namespace, self.name)
         return [] if self.type=='list' else ''
 
 class CBuild:
@@ -40,59 +45,78 @@ class CBuild:
     def __init__(self):
         pass
 
+    def __help(self):
+        methods = [name.replace('_', '-') for name in dir(self) 
+                                              if callable(getattr(self, name)) and 
+                                                 not name.startswith('_')]
+        print(MAGENTA, 'Available subcommands of cbuild:')
+        for method in methods:
+            print(CYAN, '\t%s' % (method))
+        print(RESET)
+
     def print_status(self):
         os.system('git branch -vv')
         os.system('git status -uno')
-        print('\n')
 
-    def run_command(self, args):
-        cmd = args[0].replace('-', '_')
-        if not (hasattr(CBuild, cmd) and callable(getattr(CBuild, cmd))):
-            print(RED, "'%s' is not recognized as an internal CBuild command,\n" % (cmd), 
+    def run_command(self, subcmd, args):
+        if subcmd in ['-h', '--help']:
+            return self.__help()
+
+        method = subcmd.replace('-', '_')
+        if not (hasattr(self, method) and callable(getattr(self, method))):
+            print(RED, "'%s' is not recognized as an internal CBuild command,\n" % (subcmd), 
                   YELLOW, "run 'cbuild --help' for more information.\n", RESET)
             return
-        return getattr(CBuild, cmd)(self, args)
+        return getattr(self, method)(args)
 
-    def conan_install(self, args):
-        os.system('conan install --build miss --install-folder "%s" "%s"' % (CBuild.binary_dir, CBuild.source_dir))
-        print('\n')
+    def init_packages(self, args):
+        parser = ArgumentParserNoInterrupt()
+        parser.add_argument('-rb', '--rebuild', 
+                            required=False, 
+                            default=False,
+                            action='store_true',  
+                            help='Rebuild all from sources, do not use, do not use binary packages')
+        namespace = parser.parse_args(args)
+        
+        conan_args = ['conan', 'install']
+        conan_args += ['--build', '' if namespace.rebuild else 'missing']
+        conan_args += ['--install-folder', '"%s"' % (CBuild.binary_dir)]
+        conan_args += ['"%s"' % (CBuild.source_dir)]
+        print(CYAN, '>>', ' '.join(conan_args), RESET)
+        os.system(' '.join(conan_args))
 
     def gen(self, args):
-        try:
-            iswindows = platform.system().startswith('Win')
-            parser = ArgumentParserNoInterrupt()
-            parser.add_argument('-p', '--platform', 
-                                required=False,
-                                default='win' if iswindows else 'macos', 
-                                choices=['win', 'macos', 'ios', 'android'], 
-                                help='target platform, e.g. android/ios')
-            parser.add_argument('-i', '--iset', 
-                                required=False, 
-                                default='x64', 
-                                choices=['x64', 'intel64', 'arm64'], 
-                                help='target instruction set')
-            parser.add_argument('-c', '--config', 
-                                required=False, 
-                                default='debug', 
-                                choices=['debug', 'release'], 
-                                help='target configuration type')
-            parser.add_argument('-d', '--define', 
-                                required=False,
-                                default='',
-                                action='append',
-                                help='add CMake definitions, e.g. BUILD_SHARED_LIBS:BOOL=ON')
-            namespace = parser.parse_args(args)
-        except Exception as error:
-            print(RED_INV, repr(error), RESET, end='\n\n')
-            traceback.print_exc()
-            return 1
+        iswindows = platform.system().startswith('Win')
+        parser = ArgumentParserNoInterrupt()
+        parser.add_argument('-p', '--platform', 
+                            required=False,
+                            default='win' if iswindows else 'macos', 
+                            choices=['win', 'macos', 'ios', 'android'], 
+                            help='target platform, e.g. android/ios')
+        parser.add_argument('-i', '--iset', 
+                            required=False, 
+                            default='x64', 
+                            choices=['x64', 'intel64', 'arm64'], 
+                            help='target instruction set')
+        parser.add_argument('-c', '--config', 
+                            required=False, 
+                            default='debug', 
+                            choices=['debug', 'release'], 
+                            help='target configuration type')
+        parser.add_argument('-d', '--define', 
+                            required=False,
+                            default='',
+                            action='append',
+                            help='add CMake definitions, e.g. BUILD_SHARED_LIBS:BOOL=ON')
+        namespace = parser.parse_args(args)
 
+        vsname = '"Visual Studio 16 2019" -A %s' % (namespace.iset)
         cmake_gen = ['cmake']
-        cmake_gen += ['-S', '%s'%(CBuild.source_dir)]
-        cmake_gen += ['-B', '%s'%(CBuild.binary_dir)]
-        cmake_gen += ['-G', '%s'%('Visual Studio 16 2019 -A %i'%(namespace.iset) if iswindows else 'Xcode')]
+        cmake_gen += ['-S', '"%s"'%(CBuild.source_dir)]
+        cmake_gen += ['-B', '"%s"'%(CBuild.binary_dir)]
+        cmake_gen += ['-G', '%s'%(vsname if iswindows else 'Xcode')]
         cmake_gen += ['-D' + var for var in Argument('define', 'list').of(namespace)]
-        print(CYAN, ' '.join(cmake_gen), RESET)
+        print(CYAN, '>>', ' '.join(cmake_gen), RESET)
         os.system(' '.join(cmake_gen))
         return 0
 
@@ -121,19 +145,38 @@ class CBuild:
         cmake_build += ['--target %s' % (namespace.target) if hasattr(namespace, 'target') else '']
         cmake_build += ['--config %s' % (namespace.config) if hasattr(namespace, 'config') else ''] 
         cmake_build += ['--clean-first' if hasattr(namespace, 'clean_first') else ''] 
-        print(CYAN, ' '.join(cmake_build), RESET)
+        print(CYAN, '>>', ' '.join(cmake_build), RESET)
         os.system(' '.join(cmake_build))
         return 0
 
+
+def await_input():
+    print(GREEN, getpass.getuser() + '@' + socket.gethostname() + '$', RESET, end='')
+    line = input()
+    words = line.split()
+    cmd = words[0] if len(words) >= 1 else ''
+    subcmd = words[1] if len(words) >= 2 else ''
+    args = words[2:] if len(words) >= 3 else []
+    return cmd, subcmd, args
+
 if __name__=='__main__':
+    classes = [info[0] for info in inspect.getmembers(sys.modules[__name__], 
+                                                      lambda member: inspect.isclass(member) and 
+                                                                     member.__module__==__name__)]
     cbuild = CBuild()
     cbuild.print_status()
-
+    cbuild.init_packages([])
+    
     while True:
-        print(GREEN, getpass.getuser() + '@' + socket.gethostname() + '$ ', RESET, end='')
-        parts = input().split()
-        if parts[0]=='cbuild': 
-            cbuild.run_command(parts[1:])
-        else:
-            os.system(' '.join(parts))
+        cmd, subcmd, args = await_input()
+        instance = None
+        for name in classes:
+            if not cmd==name.lower():
+                continue
+            instance = getattr(sys.modules[__name__], name)()
+            instance.run_command(subcmd, args)
+            break
+        if not instance:
+            os.system('%s %s %s' % (cmd, subcmd, ' '.join(args)))
+            continue
     exit(0)
