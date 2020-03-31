518ee8795409f563e734e6fd2b4b1ed55de268ae
wanlizhu <wanlizhu@hotmail.com>
2020-03-31 16:51:08 +0800

    add sampler

--- Engine/Experimental/GraphicsEngine/Engine/AcadRHI-Vulkan/INC/GISamplerVk.h
+++ Engine/Experimental/GraphicsEngine/Engine/AcadRHI-Vulkan/INC/GISamplerVk.h
@@ -6,16 +6,48 @@ namespace AutoCAD::Graphics::Engine
 {
     class GISamplerVk : public GIDeviceObjectVk
     {
+        friend class GISamplerBuilderVk;
         DECL_DEVICE_OBJECT(GISamplerVk)
     public:
-        
+        virtual ~GISamplerVk();
+        virtual bool IsValid() const override final;
+        virtual void SetDebugName(const char* name) const override final;
+        virtual void SetDebugTag(const DebugTag& tag) const override final;
+
+        operator const VkSampler& () const;
+        VkSamplerCreateInfo const& GetSamplerInfo() const;
+
     private:
-        GISamplerVk(const VkSamplerCreateInfo& createInfo);
+        GISamplerVk(
+            SharedPtr<GIDeviceVk> device,
+            const VkSamplerCreateInfo& createInfo
+        );
 
     private:
         VkSampler mSamplerHandle = VK_NULL_HANDLE;
         VkSamplerCreateInfo mSamplerInfo;
     };
 
+    class GISamplerBuilderVk
+    {
+    public:
+        GISamplerBuilderVk(SharedPtr<GIDeviceVk> device);
+        GISamplerBuilderVk& SetMagFilter(VkFilter filter);
+        GISamplerBuilderVk& SetMinFilter(VkFilter filter);
+        GISamplerBuilderVk& SetMipmapMode(VkSamplerMipmapMode mode);
+        GISamplerBuilderVk& SetAddressModeU(VkSamplerAddressMode addressU);
+        GISamplerBuilderVk& SetAddressModeV(VkSamplerAddressMode addressV);
+        GISamplerBuilderVk& SetAddressModeW(VkSamplerAddressMode addressW);
+        GISamplerBuilderVk& SetMipLodBias(float mipLodBias);
+        GISamplerBuilderVk& SetAnisotropy(bool enable, float maxAnisotropy);
+        GISamplerBuilderVk& SetCompareOp(bool enable, VkCompareOp compareOp);
+        GISamplerBuilderVk& SetLod(float minLod, float maxLod);
+        GISamplerBuilderVk& SetBorderColor(VkBorderColor color);
+        GISamplerBuilderVk& SetUnnormalizedCoordinates(bool value);
+        SharedPtr<GISamplerVk> Build();
 
+    private:
+        SharedPtr<GIDeviceVk> mDevice;
+        VkSamplerCreateInfo mCreateInfo = {};
+    };
 }
